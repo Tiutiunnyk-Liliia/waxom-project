182432709e8721ecd7e1ca05f60a66b9c2394326
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-09-12 18:51:19 +0300

    Complete developing tabs for the section "project" and developing section "project" done too.

--- js/main.js
+++ js/main.js
@@ -23,5 +23,16 @@ $(function () {
     });
 
 
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+
+    });
+
+
 });
 
